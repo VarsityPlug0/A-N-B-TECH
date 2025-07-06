ead28a8798429b7bd1d69d1bf957ead377a4a348
VarsityPlug0 <your-email@example.com>
2025-07-06 08:44:03 +0200

    Add STATIC_ROOT for collectstatic in production/Docker

--- iphone_store/settings.py
+++ iphone_store/settings.py
@@ -123,6 +123,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Media files (Uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
